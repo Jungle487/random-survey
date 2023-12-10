659e958fceeec12bcbc800ab58a4c0f36edc319c
Jungle487 <128108915+Jungle487@users.noreply.github.com>
2023-12-10 23:41:07 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/BMNWC9Khg6cc8ebPA',
+	'https://forms.gle/G2mb8RepTCWRgmsC7',
+	'https://forms.gle/51Y17h9mv436vifR7'
 ];
